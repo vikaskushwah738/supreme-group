e158e7e22064616a9459ca77d17054a4c7023bbe
vikaskushwah738 <vikaskushwah738@gmail.com>
2025-06-08 11:48:31 +0530

    create contect from

---
+++ components/Contact.tsx
@@ -0,0 +1,81 @@
+
+function Contact() {
+    return (
+        <section className="bg-[#006ABC] lg:flex text-[#FFFFFF] lg:px-36 md:px-16 px-5 py-20">
+            <div className="lg:w-[40%]">
+                <h2 className="text-4xl font-normal"><span className="border-b-2 border-gray-50 pb-2">Get</span> in touch</h2>
+                <ul className="space-y-5 pt-10 ">
+                    <li className="text-xl">For general enquiries</li>
+                    <li> <span className="text-xl">Address :</span> <br />
+                        110, 16th Road, Chembur, Mumbai - 400071</li>
+                    <li><span className="text-xl">Phone :</span> <br />
+                        +91 22 25208822
+                    </li>
+                    <li><span className="text-xl">Email :</span> <br />
+                        info@supremegroup.co.in</li>
+
+                </ul>
+            </div>
+            <div className="lg:w-[60%]">
+                <form className="space-y-3">
+                    {/* Full Name */}
+                    <div>
+                        <input
+                            type="text"
+                            name="fullName"
+                            placeholder="full Name"
+                            className="w-full border-b border-gray-300 px-2 py-2 focus:outline-none"
+                            required
+                        />
+                    </div>
+
+                    {/* Email */}
+                    <div >
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            className="w-full border-b border-gray-300 px-2 py-2 focus:outline-none"
+                            required
+                        />
+                    </div>
+
+                    {/* Company */}
+                    <div >
+
+                        <input
+                            type="text"
+                            name="company"
+                            placeholder="Company"
+                            className="w-full border-b border-gray-300 px-2 py-2 focus:outline-none "
+                        />
+                    </div>
+
+                    {/* Message */}
+                    <div >
+
+                        <textarea
+                            name="message"
+                            rows={4}
+                            placeholder="Message"
+                            className="w-full border-b border-gray-300 px-2 py-2 focus:outline-none"
+                            required
+                        ></textarea>
+                    </div>
+
+                    {/* Send Button */}
+                    <div className="pt-4">
+                        <button
+                            type="submit"
+                            className=" hover:bg-white hover:text-black bg-transparent border-2 border-white text-white font-semibold px-8 py-3 rounded-xl transition"
+                        >
+                            Send
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </section>
+    )
+}
+
+export default Contact
