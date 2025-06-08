c8928f6d8c54cdb25ffa8f961726cd4e898c2b2b
vikaskushwah738 <vikaskushwah738@gmail.com>
2025-06-08 20:36:09 +0530

    updates

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 function Navbar() {
     return (
         <header>
-            <nav className="bg-gray-50 fixed z-20 md:px-11 px-5 w-full flex justify-between items-center h-16">
+            <nav className="bg-[#FFFFFF] fixed z-20 md:px-11 px-5 w-full flex justify-between items-center h-16">
                 <div>
                     <Image alt="Supreme Group logo - logo"
                         src='./logo.svg'
                         width={150}
                         height={85}
-                        className="bg-white "></Image>
+                        className=""></Image>
                 </div>
             </nav>
         </header>
